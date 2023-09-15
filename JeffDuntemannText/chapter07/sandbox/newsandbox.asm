fc810d216fdfad64dc574042938de68f5cbd9c06
zakirucker <zakirucker@mac.com>
2023-09-15 05:52:39 +0200

    updates template

--- JeffDuntemannText/chapter07/sandbox/newsandbox.asm
+++ JeffDuntemannText/chapter07/sandbox/newsandbox.asm
@@ -3,7 +3,14 @@ section .text
 	global	_start
 _start:
 	nop
+	nop
 ; Put your experiments between the two nops...
+
 
 ; Put your experiments between the two nops...
 	nop
+
+; Exit the sandbox program cleanly
+	mov eax, 1		; code for exit syscall
+	mov ebx, 0		; return code
+	int 80H			; make syscall

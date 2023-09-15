7f839d788f02c04afba7d0042c7832509464cf0e
zakirucker <zakirucker@mac.com>
2023-09-15 02:23:05 +0200

    adds Duntemann Sandbox

---
+++ Sandbox/newSandbox.asm
@@ -0,0 +1,18 @@
+; created by: 	Zaki Rucker
+; description: 	a framework to try new things
+;				and explore them in a debugger.
+; date:			15 September 2023
+; inspired by: 	Assembly Language Step-By-Step
+
+section .data
+section .text
+
+	global _start
+        
+_start:
+	nop		; create fire after this line
+	
+	
+	nop		; create fire abone this line
+	
+section .bss
